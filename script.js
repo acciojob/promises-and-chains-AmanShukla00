581cf5b37f9d4789b6794ad21a5b30890703bda3
AmanShukla00 <aman17101998@gmail.com>
2025-03-06 12:05:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-document.getElementById("voteForm").addEventListener("submit", function(event) {
-            event.preventDefault();
-            
-            let name = document.getElementById("name").value;
-            let age = document.getElementById("age").value;
-            
-            if (!name || !age) {
-                alert("Please enter valid details.");
-                return;
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let name = document.getElementById("name").value.trim();
+    let age = document.getElementById("age").value.trim();
+    
+    if (!name || !age) {
+        alert("Please enter valid details.");
+        return;
+    }
+    
+    age = parseInt(age);
+    
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age > 18) {
+                resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${name}. You aren't old enough.`);
             }
-            
-            age = parseInt(age);
-            
-            new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    if (age > 18) {
-                        resolve(`Welcome, ${name}. You can vote.`);
-                    } else {
-                        reject(`Oh sorry ${name}. You aren't old enough.`);
-                    }
-                }, 4000);
-            })
-            .then(message => alert(message))
-            .catch(error => alert(error));
-        });
+        }, 4000);
+    })
+    .then(message => alert(message))
+    .catch(error => alert(error));
+});
